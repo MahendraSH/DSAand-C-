3a7fad904a967f43e3b72cefc3e75a69dc9de756
MahendrSH <maahi2001sh@gmail.com>
2023-03-01 16:13:34 +0530

    first -ve number in the the given subbary

--- adatay verdhan/sliding window problems/firstkthNevNumber.cpp
+++ adatay verdhan/sliding window problems/firstkthNevNumber.cpp
@@ -1,22 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n=8;
-    int a[]={1,2,3,4,5,6,7,8};
-    int k=3;
-    for(int i=0 ,j=k-1;i<n;i++,j--)
+    int n;
+    cout << "enter the number" << endl;
+    cin >> n;
+    int a[n];
+    cout << "Enter the eles" << endl;
+    for (int i = 0; i < n; i++)
     {
-        if(j<0 && i)
-        {
-            j=k-1;
-        }
-
+        cin >> a[i];
     }
-    for(int i=0;i<n;i++)
+    int w;
+    cout << "Enter window size" << endl;
+    cin >> w;
+    queue<int> q;
+    for(int i =0;i<n;i++)
     {
         cout<<a[i]<<" ";
     }
-
+    cout<<endl;
+    for (int i = 0, j = 0; i < n; i++)
+    {
+        if (a[i] < 0)
+        {
+            q.push(i);
+        }
+        if (i - j + 1 == w)
+        {
+            if (q.empty())
+            {
+                cout << 0<< " ";
+            }
+            else
+            {
+                cout << a[q.front()] <<" ";
+                if (q.empty()==false&&q.front()==j)
+                    q.pop();
+               
+            }
+            j++;
+        }
+    }
+    cout<<endl;
 }
